bead1eef6881d5a2c26b5e646ca2ec5ac17edfdb
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-12-24 23:58:24 +0600

    Set up a basic node express server with basic general and 'not found' page

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
@@ -9,8 +14,9 @@ app.use('/', require('./routes/root'));
 
 app.all('*', (req, res) => {
   res.status(404);
+
   if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    res.sendFile(path.join(__dirname, '/views', '/404.html'));
   } else if (req.accepts('json')) {
     res.json({ message: '404 Not Found' });
   } else {
